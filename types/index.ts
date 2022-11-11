85e92ead62e8e7f468190e819306baf2fa791d03
cheongaa <g92ki9@gmail.com>
2022-11-11 21:05:48 +0900

    chore: data type 추가

--- types/index.ts
+++ types/index.ts
@@ -1,1 +1,23 @@
-export type MbtiType = "i" | "e" | "s" | "n" | "t" | "f" | "j" | "p";
+export type MbtiElementType = 'i' | 'e' | 's' | 'n' | 't' | 'f' | 'j' | 'p';
+
+export type MbtiType =
+    | 'infp'
+    | 'infj'
+    | 'intp'
+    | 'intj'
+    | 'isfp'
+    | 'isfj'
+    | 'istp'
+    | 'istj'
+    | 'enfp'
+    | 'enfj'
+    | 'entp'
+    | 'entj'
+    | 'esfp'
+    | 'esfj'
+    | 'estp'
+    | 'estj';
+
+export type MbtiObject = {
+    [key in MbtiElementType]: number;
+};
