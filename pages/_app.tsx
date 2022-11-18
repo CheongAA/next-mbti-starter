782811fa7a26396c6c5b579a54ea07c99652caa3
cheongaa <g92ki9@gmail.com>
2022-11-18 13:28:35 +0900

    feat: google analytics 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,47 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from '../components/UI/Layout'
+import Script from 'next/script'
+import * as gtag from '../lib/gtag';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Layout>
-    <Component {...pageProps} />
-  </Layout>
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url: any) => {
+      if (router.pathname === '/result/[mbti]') {
+        gtag.pageview(url);
+      }
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router]);
+
+  return (
+    <>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script strategy="afterInteractive" src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`} />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${gtag.GA_TRACKING_ID}', {
+          page_path: window.location.pathname,
+        });
+      `
+        }}
+      />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+
+  )
 }
